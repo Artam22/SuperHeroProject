50dcf8bc43210d04ec87d439285511807b0850b9
Artam <erdemmmgurel@gmail.com>
2024-02-07 19:40:28 +0300

    Superman done V2

--- src/Superman.java
+++ src/Superman.java
@@ -2,10 +2,37 @@
 
 public class Superman extends SuperHeroes {
     // TODO: SpiderMan'deki adımları bu sınıf için de uygulayınız...
-    public Superman(String name, String gender, String aggression, String wisdom, String beauty) {
+
+    private String flyingAblity;
+    public Superman(String name, String gender, String aggression, String wisdom, String beauty, String flyingAblity) {
         super(name, gender, aggression, wisdom, beauty);
+        setFlyingAblity(flyingAblity);
     }
 
+    @Override
+    public void attack() {
+        super.attack();
+        System.out.println("Ayrıca "+getName()+" uçarken Supersonic Flight-güçlü saldırılar gerçekleştirebilir.");
+    }
 
+    @Override
+    public void exposeSecretPower() {
+        System.out.println("Superman SuperSonic Fligght hızında uçma yeteneğine sahiptir");
+    }
 
+    public String getFlyingAblity() {
+        return flyingAblity;
+    }
+
+    public void setFlyingAblity(String flyingAblity) {
+        this.flyingAblity = flyingAblity;
+    }
+
+    @Override
+    public void printInfo() {
+        super.printInfo();
+        System.out.println("Özel Güç");
+        System.out.println("-------------------");
+        System.out.println("Süpersonik uçuş= " + flyingAblity);
+    }
 }
